e1b428a14a5caa9b353576b1780d2772eeb23b3e
Carlos Nunez <carlosonunez@noreply.github.com>
2020-04-03 17:28:59 -0500

    Use Docker for frontend and backend

---
+++ start_app.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+clone() {
+  url="$1"
+  git clone "$1"
+}
+
+clone_frontend() {
+  clone "https://github.com/Razz/Project_Organizer_FrontEnd ./frontend"
+}
+
+clone_backend() {
+  clone "https://github.com/Razz/Project_Organizer_BackEnd ./backend"
+}
+
+start_app() {
+  docker-compose up frontend backend
+}
+
+for app in docker-compose git
+do
+  if ! &>/dev/null which "$app"
+  then
+    >&2 echo "ERROR: Ensure that you have '$app' installed on your machine."
+    exit 1
+  fi
+done
+
+clone_frontend &&
+  clone_backend &&
+  start_app
